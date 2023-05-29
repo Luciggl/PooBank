21c5d280446990ba3b58a2d74fbbed8fedc908a8
Luciélio <110641545+Luciggl@users.noreply.github.com>
2023-05-28 22:04:59 -0300

    Delete PooBank directory

--- PooBank/src/main/java/exception/AccountAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class AccountAlreadyExistsException extends Exception{
-    public AccountAlreadyExistsException(String msg){
-        super(msg);
-    }
-}
--- PooBank/src/main/java/exception/AccountDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class AccountDoesNotExistException extends Exception{
-    public AccountDoesNotExistException(String msg){
-        super(msg);
-    }
-}
